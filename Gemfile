b10cc523b4c1c55732c3cee53d08741661c0a061
Bob Cook <bobcook@sunrem.com>
2018-10-16 09:42:20 -0600

    Update rails and all gems

--- Gemfile
+++ Gemfile
@@ -1,39 +1,39 @@
 source 'https://rubygems.org'
-ruby '2.3.0'
+ruby '2.5.1'
 
 # Standard Rails gems
-gem 'rails', '4.2.5'
-gem 'sass-rails', '4.0.4'
-gem 'uglifier', '2.5.3'
-gem 'coffee-rails', '4.1.0'
-gem 'jquery-rails', '3.1.2'
-gem 'turbolinks', '2.5.2'
-gem 'jbuilder', '2.2.5'
-gem 'bcrypt', '3.1.9'
-gem 'octopress', '~> 3.0.0.rc'
+gem 'rails', '5.2.1'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+gem 'bcrypt'
+gem 'octopress'
 gem 'pg'
 gem 'sidekiq'
 
 # Kaminari: https://github.com/amatsuda/kaminari
-gem 'kaminari', '0.16.1'
+gem 'kaminari'
 
 # Friendly_id: https://github.com/norman/friendly_id
-gem 'friendly_id', '5.0.4'
+gem 'friendly_id'
 
 # Font-awesome: https://github.com/FortAwesome/font-awesome-sass
-gem 'font-awesome-sass', '4.2.2'
+gem 'font-awesome-sass'
 
 # Bootstrap 3: https://github.com/twbs/bootstrap-sass
-gem 'bootstrap-sass', '3.3.1.0'
+gem 'bootstrap-sass'
 
 # Figaro: https://github.com/laserlemon/figaro
 group :development, :test do
-  gem 'figaro', '1.0.0'
+  gem 'figaro'
   gem 'launchy'
 end
 
 # Devise: https://github.com/plataformatec/devise
-gem 'devise', '3.4.1'
+gem 'devise'
 gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
@@ -42,7 +42,7 @@ gem 'omniauth-google'
 gem 'omniauth-github'
 
 # Redcarpet: https://github.com/vmg/redcarpet
-gem 'redcarpet', '3.2.0'
+gem 'redcarpet'
 
 # Unicorn: http://unicorn.bogomips.org
 group :production do
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,71 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210232902) do
+ActiveRecord::Schema.define(version: 2014_12_10_232902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content_md"
-    t.text     "content_html"
-    t.boolean  "draft",        default: false
-    t.integer  "user_id"
-    t.string   "slug"
+  create_table "identities", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.text "content_md"
+    t.text "content_html"
+    t.boolean "draft", default: false
+    t.integer "user_id"
+    t.string "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.boolean  "admin",                  default: false, null: false
-    t.boolean  "locked",                 default: false, null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false, null: false
+    t.boolean "locked", default: false, null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
+    t.string "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string "name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
